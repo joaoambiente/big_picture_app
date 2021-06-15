0462eea73831531d51f4bfea804e75820b7a16d8
JoÃao Martins <joaoambiente@gmail.com>
2021-06-15 12:23:18 +0100

    Improved the streamlit frontend...

--- app.py
+++ app.py
@@ -3,43 +3,59 @@ import numpy as np
 import pandas as pd
 import datetime
 import requests
+import urllib
+import time
 
 st.markdown(''' 
 # The Big Picture 
-## Get insights from your news with sentiment analysis.
-Please provide your search terms bellow, separated by commas.
 ''')
-search_terms = st.text_input("Search terms: ")
-st.write('Requested search terms: ', search_terms)
 
-'''
-### Please provide a "start date" and an "end date" to narrow down the search. 
-'''
-date_from = st.date_input("Start date: ")
-date_to = st.date_input("End date: ")
+text = "Enhance your perspectives on the news... with AI!"
 
-date_from = str(date_from)
-date_to = str(date_to)
+t = st.empty()
+for i in range(len(text) + 1):
+    t.markdown("## %s" % text[0:i])
+    time.sleep(0.0001)
 
-## Once we have these, let's call our API in order to retrieve a prediction
-# See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-# 🤔 How could we call our API ? Of course... The `requests` package 💡
+query = st.text_input("Search terms (english only): ")
+# st.write("You've requested these search terms:  ", query)
 
-url = 'https://github.com/archifreitas/bg_api'
+st.markdown('''  
+## Advanced query:
+''')
 
-# 2. Let's build a dictionary containing the parameters for our API...
+date_from = st.date_input("Please select how far back you want to get the news from: ")
+date_from = date_from.strftime("%Y-%m-%d")
 
-# dict of params
-params = {
-        "q": search_terms,
-        "from": date_from,
-        "to": date_to,
+# st.write("You've requested these search terms:  ", date_from)
+
+st.markdown('''#### Title''')
+title = st.text_input("Select search terms within a title (english only): ", key=1)
+
+st.markdown('''#### Sources''')
+source = st.text_input('''Input terms separated by commas (maximum 20) 
+for the news sources you want headlines from (english sources only) : ''', key=2)
+
+st.markdown('''#### Domain''')
+label = st.text_input("Input domains separated by commas (eg bbc.co.uk, techcrunch.com, engadget.com) to restrict the search: ", key=3)
+
+# URL for our API...
+url = 'https://github.com/archifreitas/bg_api/'
+
+# Dictionary containing the parameters for our News API (news obtained from newsAPI)...
+endpoint = "search"
+news_params = {
+        "query": query,
+        "title": title,
+        "source": source,
+        "label" : label,
+        "date_from": date_from
         }
 
-# Calling our Big Picture API using the `requests` package and return a value with a button
+# Calling our Big Picture API using the `requests` package and returning a value with a button
 
-if st.button('Get news!'):
-    response = requests.get(url, params=params)
+if st.button('Get news!', key=1):
+    response = requests.get(url + endpoint, params=news_params)
     response_json = response.json()
 
     # Retrieving the prediction from the **JSON** returned by the API...
@@ -51,8 +67,17 @@ else:
     st.write('Click this button to get a list of news based on your search parameters.')
 
 
-if st.button('Get sentiment_analysis_predicition!'):
-    response = requests.get(url, params=params)
+# my_dict = news_list[0]
+my_dict = {}
+
+# Dictionary containing the parameters for our Sentiment Analysis (from or model)...
+endpoint = "predict"
+sentiment_params = {
+        "sample": my_dict
+            }
+
+if st.button('What is the sentiment of this news article, positive or negative?', key=2):
+    response = requests.get(url, params=sentiment_params)
     response_json = response.json()
 
     # Retrieving the prediction from the **JSON** returned by the API...
@@ -64,6 +89,10 @@ else:
     st.write("Click this button to retrieve a sentiment analysis on the text you've selected.")
 
 # get news list based on API request (considering the predicted tag, cluster aggregation and sentiment analysis result)
+
+
+st.markdown('''#### A want to see an article with a different sentiment score: ''')
+
 @st.cache
 def get_news_list():
     print('News List')
@@ -80,3 +109,6 @@ option = st.slider('Select a modulus', 0, 100, 50)
 filtered_df = df[df['Sentiment Analysis result'] % option == 0]
 
 st.write(filtered_df)
+
+
+
