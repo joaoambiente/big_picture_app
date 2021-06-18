1bb1fa1ab1e6333fba8faa02c85446d5859e3e6f
JoÃao Martins <joaoambiente@gmail.com>
2021-06-18 11:34:30 +0100

    4th final heroku push

--- app.py
+++ app.py
@@ -36,15 +36,15 @@ def get_news(search='simple'):
         else:
             session_state.checkboxed2 = True
         ## Retrieving the prediction from the **JSON** returned by the API...
-        #url = "https://api-s4zpk52g3a-ew.a.run.app/"
-        #endpoint = "search"
-        #response = requests.get(url + endpoint, params=news_params)
-        #news_list = response.json()
+        url = "https://api-s4zpk52g3a-ew.a.run.app/"
+        endpoint = "search"
+        response = requests.get(url + endpoint, params=news_params)
+        news_list = response.json()
 
         ## Retrieving the prediction from the **JSON** placeholder...
-        get_sources = open('./data/example_search_output.json',) 
-        news_list = json.load(get_sources)
-        get_sources.close()
+        # get_sources = open('./data/example_search_output.json',) 
+        # news_list = json.load(get_sources)
+        # get_sources.close()
 
         hr = f'<hr class="divider"></hr>'
         st.markdown(hr, unsafe_allow_html=True)
@@ -81,18 +81,18 @@ def get_news(search='simple'):
                 with col1:
                     if st.button("Analyse", key=keys*modifier):
 
-                        #url = "http://35.184.150.29:8080/predict"
-                        #response = requests.post(url, json=news)
-                        #data = response.json()
-                        #topic = data['topic']
-                        #data_df = json.loads(data['data'])
-                        #data_df = pd.DataFrame(data_df)
-
-                        get_sources = open('./data/response_1623996348612.json')
-                        data = json.load(get_sources)
+                        url = "http://35.184.150.29:8080/predict"
+                        response = requests.post(url, json=news)
+                        data = response.json()
+                        topic = data['topic']
                         data_df = json.loads(data['data'])
                         data_df = pd.DataFrame(data_df)
-                        topic = data['topic']
+
+                        # get_sources = open('./data/response_1623996348612.json')
+                        # data = json.load(get_sources)
+                        # data_df = json.loads(data['data'])
+                        # data_df = pd.DataFrame(data_df)
+                        # topic = data['topic']
 
                         # with col2:
                         #     if data_df.iloc[0,5] > data_df.SA.mean():
